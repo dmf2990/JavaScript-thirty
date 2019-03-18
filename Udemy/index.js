b6807122d54e8f119e50f7444d92e8021caf7f80
Drew Franklin <drewfranklin@drews-mbp.attlocal.net>
2019-03-17 20:49:56 -0400

    started udemy-- very basics

---
+++ Udemy/index.js
@@ -0,0 +1,87 @@
+// //vars & data types
+
+// // console.log('drew');
+
+// let firstName = 'Drew'
+// let lastName = 'Franklin';
+// console.log(lastName);
+
+// let age = 28;
+
+// let fullAge = true; 
+
+// console.log(fullAge);
+
+// //undefined var
+// let job;
+// console.log(job); 
+
+// //later defined
+// job = 'Teacher';
+// console.log(job);
+
+//********************************************/
+//variable mutation and type coercion
+
+// let firstNamee = 'Dale';
+// let age = 34;
+
+// //concat
+// console.log(firstNamee + ' ' + age);
+
+// //declare multiple variables at once
+// var job, isMarried;  
+// job = 'teacher';
+// isMarried = false;
+
+// console.log(firstNamee + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//age = 'it knows that this is a new var and its a string'; 
+
+//alert('yes'); 
+
+//********************************************/
+//********************************************/
+
+//let UserInputName = prompt('question to ask user to answer?')
+
+//this assigns whatever they type to userInputName variable
+
+//console.log(UserInputName);
+
+//try
+
+
+//math
+//*********************/
+
+var year, yearDrew, yearKate;
+now = 2019;
+ageDrew = 29;
+ageKate = 27;
+
+now = 2019;
+yearDrew = now - ageDrew;
+yearKate = now - ageKate;
+
+console.log(yearKate);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//logical operators
+
+let drewOlder = ageDrew > ageKate;
+console.log(drewOlder);
+
+
+
+
+
+
+
+
+
+
+
